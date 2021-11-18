5e8bf68a82d97c242b3abf2838e1c75d6c2d543c
huawei laptop <huawei@laptop.com>
2021-11-18 13:56:47 +0800

    change id from -data.sql via id-worker

--- statistic/src/main/resources/sql/statistic-data.sql
+++ statistic/src/main/resources/sql/statistic-data.sql
@@ -1,9 +1,13 @@
 SET FOREIGN_KEY_CHECKS=0;
 
+DELETE FROM `sys_perm_group` WHERE id='1461211162149453826';
 INSERT INTO `sys_perm_group` (`id`, `org_id`, `pid`, `identifier`, `name`) VALUES
-('1001', '100000000000000001', '100000000000000101', 'report', '报表管理');
-INSERT INTO `sys_perm_group` (`id`, `org_id`, `pid`, `identifier`, `name`) VALUES
-('1001001', '100000000000000001', '1001', 'report.management', '报表管理');
+('1461211162149453826', '100000000000000001', '100000000000000101', 'report', '报表管理');
 
+DELETE FROM `sys_perm_group` WHERE id='1461211250296967169';
+INSERT INTO `sys_perm_group` (`id`, `org_id`, `pid`, `identifier`, `name`) VALUES
+('1461211250296967169', '100000000000000001', '1461211162149453826', 'report.management', '报表管理');
+
+DELETE FROM `sys_perm` WHERE id='1461211327577051138';
 INSERT INTO `sys_perm` (`id`, `group_id`, `identifier`, `name`, `tag`) VALUES
-('1001001001', '1001001', 'defaultReport.view', '默认报表权限', '0');
+('1461211327577051138', '1461211250296967169', 'defaultReport.view', '默认报表权限', '0');
